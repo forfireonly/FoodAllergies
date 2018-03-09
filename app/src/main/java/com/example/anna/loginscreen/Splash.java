178ecd13b3348f9e2db902bec726ba3c27c5ab47
Firexit <workwithpurpose@yahoo.com>
2018-03-09 16:56:19 -0600

    Flash Screen

---
+++ app/src/main/java/com/example/anna/loginscreen/Splash.java
@@ -0,0 +1,19 @@
+package com.example.anna.loginscreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
